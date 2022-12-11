fdfa9dd06ffcd2a2302dcbafc8212d3b3df7a496
Ranel John Tormis <62842207+R-esentful@users.noreply.github.com>
2022-12-11 17:46:53 +0800

    updated route

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,3 @@
-
 from app import app
 from flask import render_template
 
@@ -7,32 +6,32 @@ from flask import render_template
 @app.route('/')
 @app.route('/home')
 def home_page():
-    return render_template('Landing/landing_page.jinja')
+    return render_template('Landing/landing_page.html')
 
 @app.route('/create-account/student')
 def student_page():
-    return render_template('Create&Login/create_student.jinja')
+    return render_template('Create&Login/create_student.html')
 
 @app.route('/create-account/professor')
 def professor_page():
-    return render_template('Create&Login/create_professor.jinja')
+    return render_template('Create&Login/create_professor.html')
 
 @app.route('/login')
 def login_page():
-    return render_template('Create&Login/login.jinja')
+    return render_template('Create&Login/login.html')
 
 @app.route('/dashboard')
 def dashboard_page():
-    return render_template('Dashboard/main.jinja')
+    return render_template('Dashboard/main.html')
 
 @app.route('/classroom')
 def classroom_page():
-    return render_template('Dashboard/classroom.jinja')
+    return render_template('Dashboard/classroom.html')
 
 @app.route('/profile')
 def profile_page():
-    return render_template('Dashboard/profile.jinja')
+    return render_template('Dashboard/profile.html')
 
 @app.route('/upload')
 def upload_page():
-    return render_template('Dashboard/upload.jinja')
+    return render_template('Dashboard/upload.html')
