1806ebd4c688605a072f3ad1e1bf7c12bd0c57b7
Ranel John Tormis <62842207+R-esentful@users.noreply.github.com>
2022-12-13 12:55:32 +0800

    updated init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,19 @@
+# Securing Secret Key & Database Key
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
 
 app = Flask(__name__,template_folder = 'template')
+# Initialize secret key for user session protection
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+
+
+# Initialize database
+#db = SQLAlchemy(app)
 
 from app import routes
