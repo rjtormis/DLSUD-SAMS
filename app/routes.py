95377f9df3046d2d10e92e13a1225b8de89fb5fb
Ranel John Tormis <62842207+R-esentful@users.noreply.github.com>
2022-12-17 14:40:04 +0800

    updated routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,32 @@
-from app import app
-from flask import render_template
+from app import app,db
+from flask import render_template,request,redirect,url_for,flash,get_flashed_messages
 
 
+from app.models.forms import RegisterStudent
+from app.models.models import Student
 
 @app.route('/')
 @app.route('/home')
 def home_page():
     return render_template('Landing/landing_page.html')
 
-@app.route('/create-account/student')
+@app.route('/create-account/student',methods = ['GET','POST'])
 def student_page():
-    return render_template('Create&Login/create_student.html')
+    student_form = RegisterStudent()
+
+    if request.method == 'POST':
+        if student_form.validate_on_submit():
+            student_account = Student(idNumber = student_form.idNumber.data,firstName = student_form.firstName.data,middleName = student_form.middleName.data,lastName = student_form.lastName.data,emailAddress = student_form.emailAddress.data,password = student_form.password1.data)
+            db.session.add(student_account)
+            db.session.commit()
+        if student_form.errors != {}:
+            # Iterates through the error message
+            for err_msg in student_form.errors.values():
+                print(err_msg)
+        return redirect(url_for('student_page'))
+
+    else:
+        return render_template('Create&Login/create_student.html',student_form = student_form)
 
 @app.route('/create-account/professor')
 def professor_page():
