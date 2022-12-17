b27c1897f180afa3e3fef44f68d869c0dc6543ab
Ranel John Tormis <62842207+R-esentful@users.noreply.github.com>
2022-12-17 14:38:21 +0800

    Updated init

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__,template_folder = 'template')
@@ -15,6 +15,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
 
 # Initialize database
 db = SQLAlchemy(app)
+
+# Initialize BCrypt
+bcrypt = Bcrypt(app)
+
+
 
 # Essential imports!
 from app import routes
