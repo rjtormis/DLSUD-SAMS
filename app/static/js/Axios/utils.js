998c419c95c562468a34f75165402e8bbf48111f
Ranel John Tormis <62842207+R-esentful@users.noreply.github.com>
2022-12-31 21:24:07 +0800

    Added utilities

--- app/static/js/Axios/utils.js
+++ app/static/js/Axios/utils.js
@@ -8,3 +8,204 @@ export function debounce(cb, delay = 1000) {
 		}, delay);
 	};
 }
+
+// TODO: FIX PASSWORD AND CONFIRM PASSWORD STLYE
+// ISSUE DATE: DECEMBER 31 2022
+
+// REGEX
+const nameRegex = '^[A-Za-z ]+$';
+const passwordRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g;
+
+const all_input_group = document.querySelectorAll('.input-group-text');
+export const input_group_id = all_input_group[0];
+export const input_group_email = all_input_group[1];
+export const input_group_password1 = all_input_group[2];
+export const input_group_password2 = all_input_group[3];
+// ID
+export const id = document.querySelector('#id_input');
+export const idLabel = document.querySelector('label[for = "id_input"]');
+
+// First name
+export const firstName = document.querySelector('#first_name');
+export const firstNameLabel = document.querySelector('label[for="first_name"]');
+const current_label_firstname = firstNameLabel.textContent;
+
+// Middle Initial
+export const middleName = document.querySelector('#initial');
+export const middleNameLabel = document.querySelector('label[for="initial"]');
+const current_label_middlename = middleNameLabel.textContent;
+
+// Last name
+export const lastName = document.querySelector('#last_name');
+export const lastNameLabel = document.querySelector('label[for="last_name"]');
+const current_label_lastname = lastNameLabel.textContent;
+
+// Email Address
+export const email = document.querySelector('#email_input');
+export const emailLabel = document.querySelector('label[for="email_input"]');
+// Password 1
+export const password1 = document.querySelector('#password_input');
+export const password1Label = document.querySelector('label[for="password_input"]');
+// Password 2 (Confirm)
+export const password2 = document.querySelector('#password_confirm');
+export const password2Label = document.querySelector('label[for="password_confirm"]');
+
+// Submit
+export const submit = document.querySelector('.registerBtn');
+
+// REGEX CHECKER FOR INPUT IN LAST NAME , MIDDLE I, FIRST NAME
+export function validateInput(input, element_var, label_var, characters, default_content) {
+	const regex = new RegExp(nameRegex);
+
+	if (input.search(/[0-9]/) !== -1) {
+		element_var.classList.add('is-invalid');
+		element_var.classList.remove('is-valid');
+		label_var.textContent = 'Invalid';
+		label_var.style.color = 'red';
+		label_var.style.fontWeight = 'bold';
+	} else {
+		if (regex.test(input) && input !== '' && input.length >= characters) {
+			element_var.classList.add('is-valid');
+			element_var.classList.remove('is-invalid');
+			label_var.textContent = `${default_content}`;
+			label_var.style.color = 'green';
+			label_var.style.fontWeight = 'bold';
+		} else if (regex.test(input) && input !== '' && input.length < characters) {
+			element_var.classList.add('is-invalid');
+			element_var.classList.remove('is-valid');
+			label_var.textContent = `Too short`;
+			label_var.style.color = 'red';
+			label_var.style.fontWeight = 'bold';
+		}
+	}
+}
+
+// Validate password
+export function validatePassword() {
+	const password = password1.value;
+	const regex = new RegExp(passwordRegex);
+
+	const hasSpecialCharacter = regex.test(password);
+
+	if (password === '') {
+		password1Label.textContent = 'Password';
+		password1Label.color = 'red';
+		password1.classList.remove('is-invalid');
+		password1.classList.remove('is-valid');
+	} else {
+		if (hasSpecialCharacter && password.length >= 8) {
+			password1.classList.add('is-valid');
+			password1.classList.remove('is-invalid');
+			password1Label.textContent = 'Password';
+		} else if (hasSpecialCharacter && password.length < 8) {
+			password1.classList.add('is-invalid');
+			password1.classList.remove('is-valid');
+			password1Label.textContent = 'Password length is too short.';
+		} else {
+			password1.classList.add('is-invalid');
+			password1.classList.remove('is-valid');
+			password1Label.textContent = 'Password should have atleast 1 special character';
+		}
+	}
+
+	const confirmed_password = password2.value;
+	if (confirmed_password == '') {
+		password2.classList.remove('is-invalid');
+		password2.classList.remove('is-valid');
+		password2Label.textContent = 'Confirm Password';
+	} else if (confirmed_password !== '' && password !== confirmed_password) {
+		password2.classList.add('is-invalid');
+		password2.classList.remove('is-valid');
+		password2Label.textContent = 'Password do not match';
+	} else if (confirmed_password !== '' && password == confirmed_password) {
+		password2.classList.add('is-valid');
+		password2.classList.remove('is-invalid');
+		password2Label.textContent = 'Password Matched';
+	}
+}
+
+// Remove all elements
+export function removeAll() {
+	firstName.classList.remove('is-invalid');
+	middleName.classList.remove('is-invalid');
+	lastName.classList.remove('is-invalid');
+	id.classList.remove('is-invalid');
+	email.classList.remove('is-invalid');
+	password1.classList.remove('is-invalid');
+	password2.classList.remove('is-invalid');
+
+	firstName.classList.remove('is-valid');
+	middleName.classList.remove('is-valid');
+	lastName.classList.remove('is-valid');
+	id.classList.remove('is-valid');
+	email.classList.remove('is-valid');
+	password1.classList.remove('is-valid');
+	password2.classList.remove('is-valid');
+
+	firstNameLabel.textContent = 'First name';
+	middleNameLabel.textContent = 'Middle I.';
+	lastNameLabel.textContent = 'Last name';
+	emailLabel.textContent = 'Email Address';
+	password1Label.textContent = 'Password';
+	password2Label.textContent = 'Confirm Password';
+
+	firstName.textContent = '';
+	middleName.textContent = '';
+	lastName.textContent = '';
+	id.textContent = '';
+	email.textContent = '';
+	password1.textContent = '';
+	password2.textContent = '';
+}
+
+// firstName Event Listener
+firstName.addEventListener('input', (e) => {
+	const first = firstName.value;
+	if (first === '') {
+		firstName.classList.remove('is-invalid');
+		firstName.classList.remove('is-valid');
+		firstNameLabel.textContent = 'First name';
+		firstNameLabel.style.color = '#212529';
+		firstNameLabel.style.fontWeight = '400';
+	} else {
+		validateInput(first, firstName, firstNameLabel, 2, current_label_firstname);
+	}
+});
+
+// middleName Event Listener
+middleName.addEventListener('input', (e) => {
+	const middle = middleName.value;
+	if (middle === '') {
+		middleName.classList.remove('is-invalid');
+		middleName.classList.remove('is-valid');
+		middleNameLabel.textContent = 'Middle I.';
+		middleNameLabel.style.color = '#212529';
+		middleNameLabel.style.fontWeight = '400';
+	} else {
+		validateInput(middle, middleName, middleNameLabel, 1, current_label_middlename);
+	}
+});
+
+// lastName Event Listener
+lastName.addEventListener('input', (e) => {
+	const last = lastName.value;
+	if (last === '') {
+		lastName.classList.remove('is-invalid');
+		lastName.classList.remove('is-valid');
+		lastNameLabel.textContent = 'Last name';
+		lastNameLabel.style.color = '#212529';
+		lastNameLabel.style.fontWeight = '400';
+	} else {
+		validateInput(last, lastName, lastNameLabel, 4, current_label_lastname);
+	}
+});
+
+// password1 Event Listener
+password1.addEventListener('input', validatePassword);
+// password2 Event Listener
+password2.addEventListener('input', validatePassword);
+// Submit Event Listener
+submit.addEventListener('click', (e) => {
+	e.preventDefault();
+	removeAll();
+});
