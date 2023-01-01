df229d424abc3d00a85f407cca601be9315bc505
Ranel John Tormis <62842207+R-esentful@users.noreply.github.com>
2023-01-01 21:11:27 +0800

    Updated routes

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from app import app,db
 from flask import render_template,request,redirect,url_for,flash,get_flashed_messages,jsonify
 from flask_login import login_user,login_required,current_user,logout_user
-
+from wtforms.validators import ValidationError
 # All Forms
 from app.models.forms import StudentForm,FacultyForm,LoginForm,SectionForm,SubjectForm
 
@@ -36,8 +36,8 @@ def student_page():
                                     lastName = student_form.lastName.data,
                                     fullName = f'{student_form.firstName.data} {student_form.middleName.data+"."} {student_form.lastName.data} ',
                                     emailAddress = student_form.emailAddress.data,
-                                    password = student_form.password1.data)            
-                
+                                    password = student_form.password1.data)
+
             db.session.add(student_account)
             db.session.commit()
 
@@ -45,12 +45,10 @@ def student_page():
 
 
         if student_form.errors != {}:
-
-            for err_msg in student_form.errors.values():
-
-                print(err_msg)
-
-    # GET Request.    
+            for field, errors in student_form.errors.items():
+                for error in errors:
+                    print(f"Error in field {field}: {error}")
+    # GET Request.
     return render_template('Create&Login/create_student.html',student_form = student_form)
 
 # TODO: ADD FLASHES, ERROR HANDLING
@@ -65,7 +63,7 @@ def faculty_page():
     collegiates = [(row.collegiate_name,row.collegiate_shorten) for row in db.session.query(Collegiate).all()]
 
     faculty_form.collegiate.choices = collegiates
-    
+
     # Iterate through the Collegiates and returns the Collegiate ID base on the user choice.
     # Related to the code above.
     collegiate_id = Collegiate.query.filter_by(collegiate_name = faculty_form.collegiate.data).first()
@@ -82,7 +80,7 @@ def faculty_page():
                                     password = faculty_form.password1.data,
                                     collegiate_id = collegiate_id.collegiate_id ,
                                     birthDate = faculty_form.birthDate.data)
-            
+
             db.session.add(faculty_account)
             db.session.commit()
 
@@ -91,20 +89,20 @@ def faculty_page():
         if faculty_form.errors != {}:
 
             for err,err_msg in faculty_form.errors.items():
-                
-                print(err,err_msg)
-        
 
-    if request.method == 'GET':  
+                print(err,err_msg)
+
+
+    if request.method == 'GET':
 
         return render_template('Create&Login/create_faculty.html',faculty_form = faculty_form)
 
 @app.route('/login',methods = ['GET','POST'])
 def login_page():
-    
+
     # Login Form
     login_form = LoginForm()
-    
+
     if request.method == 'POST':
 
         if login_form.validate_on_submit():
@@ -114,10 +112,10 @@ def login_page():
 
             # Checks if the user exists and checks if the password is correct.
             if user_login and user_login.check_password(input_password = login_form.password.data):
-                
-                # Logins the user 
+
+                # Logins the user
                 login_user(user_login)
-                
+
                 return redirect(url_for('dashboard_page'))
 
     return render_template('Create&Login/login.html',login_form = login_form)
@@ -150,62 +148,50 @@ def section_list():
 
         if section_form.validate_on_submit():
 
-            addSection = Section(faculty_id = current_user.faculty_id,section_name =f'{section_form.courseName.data} {section_form.year.data}{section_form.section.data}',collegiate_id = current_user.collegiate_id)
-            
-            # Checks the availability of the section
-            if addSection.checkSection(sectionName = addSection.section_name):
-                
-                # Creates a directory for file storage in the specific section
-                try:
-                    # Using Absolute Path & Relative Path
-                    cwd = os.getcwd()
-                    target = 'app/static/files/section'
-                    full = os.path.relpath(target,cwd)
-                    
-                    # Final Location
-                    final = os.path.join(full,f'{addSection.section_name}')
+            addSection = Section(faculty_id = current_user.faculty_id,
+                                section_name =f'{section_form.courseName.data} {section_form.year.data}{section_form.section.data}',
+                                collegiate_id = current_user.collegiate_id)             # Creates a directory for file storage in the specific section
 
-                    # Create Folder / Directory
-                    os.makedirs(final)
-                except:
-                    print('File Exists')
+            try:
+                # Using Absolute Path & Relative Path
+                cwd = os.getcwd()
+                target = 'app/static/files/section'
+                full = os.path.relpath(target,cwd)
+                # Final Location
+                final = os.path.join(full,f'{addSection.section_name}')
+                # Create Folder / Directory
+                os.makedirs(final)
+            except:
+                print('File Exists')
 
-                # Gets the uploaded file
-                file = section_form.file.data
+            # Gets the uploaded file
+            file = section_form.file.data
 
-                # Checks if there is a file and if the file extension is correct e.g JPG,PNG
-                if file and addSection.checkExtension(filename=file.filename):
-
-                    # Change the file name according to the section name
-                    file.filename = addSection.changeFileName(filename= file.filename,sectionName=addSection.section_name)
-                    
-                    # Save image to the designated file directory
-                    file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER']+f'/section/{addSection.section_name}',secure_filename(file.filename)))
-                    
-                    # Set Image Location referring to the file directory
-                    imageLocation = f'../../static/files/section/{addSection.changeDirectoryName(directory=addSection.section_name)}/{file.filename}'
-                    
-                    # Save to Database. 
-                    addSection.section_image_loc = imageLocation       
-                else:
-                    print('File is not allowed')
-                
-                db.session.add(addSection)
-                db.session.commit()
-
-                return redirect(url_for('section_list'))
+            # Checks if there is a file and if the file extension is correct e.g JPG,PNG
+            if file:
+                # Change the file name according to the section name
+                file.filename = addSection.changeFileName(filename= file.filename,sectionName=addSection.section_name)
+                # Save image to the designated file directory
+                file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER']+f'/section/{addSection.section_name}',secure_filename(file.filename)))
+                # Set Image Location referring to the file directory
+                imageLocation = f'../../static/files/section/{addSection.changeDirectoryName(directory=addSection.section_name)}/{file.filename}'
+                # Save to Database.
+                addSection.section_image_loc = imageLocation
             else:
-                print('error occured in db.')
-            
-        
-        if section_form.errors != {}:
+                # If user doesn't want to have a custom image, set to default
+                imageLocation = f'../../static/img/clbg.jpg'
+                addSection.section_image_loc = imageLocation
 
-            for err_msg in section_form.errors.values():
-                
+            db.session.add(addSection)
+            db.session.commit()
+
+        if section_form.errors != {}:
+             for err_msg in section_form.errors.values():
                 print(err_msg)
 
-    if request.method == 'GET':
+        return redirect(url_for('section_list'))
 
+    if request.method == 'GET':
         return render_template('Dashboard/Classroom.html',section_form = section_form,sections = section)
 
 # API END POINT FOR SPECIFIC SECTION
@@ -215,14 +201,15 @@ def section_page(section_name):
 
     subject_form = SubjectForm()
     
+
     section = Section.query.filter_by(section_name = section_name).first()
-    
+    subject_form.section.data = section.section_id
+
     subjects = Subject.query.filter_by(section_id = section.section_id)
 
     if request.method == 'POST':
-
+        # subject_form.subject_code = subjects.subject_code
         if subject_form.validate_on_submit():
-            
             addSubject = Subject(section_id = section.section_id,
                                 faculty_id = current_user.faculty_id,
                                 subject_code = Subject.id_generator(Subject,4),
@@ -231,52 +218,48 @@ def section_page(section_name):
                                 subject_start = Subject.changeTime(Subject,subject_form.start.data),
                                 subject_end = Subject.changeTime(Subject,subject_form.end.data),
                                 subject_full = f'{subject_form.day.data} {Subject.changeTime(Subject,subject_form.start.data)} TO {Subject.changeTime(Subject,subject_form.end.data)}')
-        
-            if addSubject and addSubject.checkSubject(addSubject.subject_name,addSubject.subject_code):
-                
-                try:
-                    # Using Absolute Path & Relative Path
-                    cwd = os.getcwd()
-                    target = f'app/static/files/section/{section.section_name}/subject'
-                    full = os.path.relpath(target,cwd)
-                    
-                    # Final Location
-                    final = os.path.join(full,f'{addSubject.subject_name}')
-        
-                    # Create Folder / Directory
-                    os.makedirs(final)
-                except:
-                    print('File Exists')
-                
 
-                file = subject_form.file.data
+            try:
+                # Using Absolute Path & Relative Path
+                cwd = os.getcwd()
+                target = f'app/static/files/section/{section.section_name}/subject'
+                full = os.path.relpath(target,cwd)
 
-                if file and addSubject.checkExtension(filename = file.filename):
-                    
-                    file.filename = addSubject.changeFileName(filename=file.filename,subjectName = addSubject.subject_name)
-                    
-                    file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER']+f'/section/{section.section_name}/subject/{addSubject.subject_name}',secure_filename(file.filename)))
+                # Final Location
+                final = os.path.join(full,f'{addSubject.subject_name}')
 
-                    imageLocation = f'../../static/files/section/{addSubject.changeDirectoryName(section.section_name)}/subject/{addSubject.changeDirectoryName(addSubject.subject_name)}/{file.filename}'
-                    addSubject.subject_image_loc = imageLocation
+                # Create Folder / Directory
+                os.makedirs(final)
+            except:
+                print('File Exists')
 
-                  
-                else:
-                    imageLocation = f'../../static/img/clbg.jpg'
-                    addSubject.subject_image_loc = imageLocation
-            
-        
+
+            file = subject_form.file.data
+
+            if file:
+
+                file.filename = addSubject.changeFileName(filename=file.filename,subjectName = addSubject.subject_name)
+
+                file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER']+f'/section/{section.section_name}/subject/{addSubject.subject_name}',secure_filename(file.filename)))
+
+                imageLocation = f'../../static/files/section/{addSubject.changeDirectoryName(section.section_name)}/subject/{addSubject.changeDirectoryName(addSubject.subject_name)}/{file.filename}'
+                addSubject.subject_image_loc = imageLocation
+
             else:
-                print('Subject already exist!')
-            
+                imageLocation = f'../../static/img/clbg.jpg'
+                addSubject.subject_image_loc = imageLocation
+
+
             db.session.add(addSubject)
             db.session.commit()
-        
+
+        if subject_form.errors != {}:
+             for err_msg in subject_form.errors.values():
+                print(err_msg)
         # Redirect back.
         return redirect(url_for('section_page',section_name = section_name))
 
     if request.method == 'GET':
-
         return render_template('Dashboard/Subject Page.html',section = section,subject_form = subject_form,subjects = subjects)
 
 # API END POINT FOR SPECIFIC SUBJECT
