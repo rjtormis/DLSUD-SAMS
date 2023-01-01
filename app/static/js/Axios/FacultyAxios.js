83c24ef457110f7f7492967c6b366b1ed69e0a4f
Ranel John Tormis <62842207+R-esentful@users.noreply.github.com>
2023-01-01 21:11:51 +0800

    Updated Faculty Axios

--- app/static/js/Axios/FacultyAxios.js
+++ app/static/js/Axios/FacultyAxios.js
@@ -1,10 +1,46 @@
 import { debounce } from './utils.js';
 import { firstName, firstNameLabel, middleName, middleNameLabel, lastName, lastNameLabel } from './utils.js';
 import { id, idLabel, email, emailLabel, password1, password1Label, password2, password2Label } from './utils.js';
-
+import { all_input_group, input_group_email } from './utils.js';
 // ============================================================================
 // 							FETCH API FOR BACKEND QUERY
 // ============================================================================
+
+// Collegiate
+const collegiate = document.querySelector('#collegiate_input');
+const collegiateLabel = document.querySelector('label[for="collegiate_input"]');
+// Birthdate
+const birthDate = document.querySelector('#date_input');
+const birthDateLabel = document.querySelector('label[for="date_input"]');
+
+const input_group_collegiate = all_input_group[0];
+const input_group_birthdate = all_input_group[1];
+
+if (collegiate.value) {
+	collegiate.style.borderColor = 'green';
+	collegiateLabel.style.color = 'green';
+	collegiateLabel.style.fontWeight = '400';
+	input_group_collegiate.style.borderColor = 'green';
+}
+
+birthDate.addEventListener('input', (e) => {
+	const date = birthDate.value;
+	if (date === '') {
+		birthDate.style.borderColor = '#ced4da';
+
+		birthDateLabel.style.color = '#212529';
+		birthDateLabel.style.fontWeight = '400';
+
+		input_group_birthdate.style.borderColor = '#ced4da';
+	} else {
+		birthDate.style.borderColor = 'green';
+
+		birthDateLabel.style.color = 'green';
+		birthDateLabel.style.fontWeight = '400';
+
+		input_group_birthdate.style.borderColor = 'green';
+	}
+});
 
 const debounce_email = debounce((email_input) => {
 	axios
@@ -20,16 +56,31 @@ const debounce_email = debounce((email_input) => {
 		.then((res) => {
 			if (res.data.avail === true) {
 				emailLabel.textContent = 'Email Address is available';
+				emailLabel.style.fontWeight = '400';
+				emailLabel.style.color = 'green';
+
 				email.classList.add('is-valid');
 				email.classList.remove('is-invalid');
+
+				input_group_email.style.borderColor = 'green';
 			} else if (res.data.avail === false) {
 				emailLabel.textContent = 'Email Address is already taken';
+				emailLabel.style.fontWeight = '400';
+				emailLabel.style.color = 'red';
+
 				email.classList.add('is-invalid');
 				email.classList.remove('is-valid');
+
+				input_group_email.style.borderColor = 'red';
 			} else if (res.data.avail === 'invalid') {
 				emailLabel.textContent = 'Must be a DLSUD email';
+				emailLabel.style.fontWeight = '400';
+				emailLabel.style.color = 'red';
+
 				email.classList.add('is-invalid');
 				email.classList.remove('is-valid');
+
+				input_group_email.style.borderColor = 'red';
 			}
 		})
 		.catch((err) => {
@@ -40,9 +91,15 @@ const debounce_email = debounce((email_input) => {
 email.addEventListener('input', (e) => {
 	const email_input = email.value;
 	if (email_input == '') {
+		emailLabel.textContent = 'Email Address';
+		emailLabel.classList.add('border-start-0');
+		emailLabel.style.color = '#212529';
+		emailLabel.style.fontWeight = '400';
+
 		email.classList.remove('is-invalid');
 		email.classList.remove('is-valid');
-		emailLabel.textContent = 'Email Address';
+
+		input_group_email.style.borderColor = '#ced4da';
 	} else {
 		debounce_email(email_input);
 	}
