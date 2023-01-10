08dce32637a8d564deb7c0f6111369da9ea02358
Ranel John Tormis <62842207+R-esentful@users.noreply.github.com>
2023-01-10 18:26:17 +0800

    Updated subjectpageaxios

--- app/static/js/Axios/SubjectPageAxios.js
+++ app/static/js/Axios/SubjectPageAxios.js
@@ -1,39 +1,46 @@
 import { debounce } from './utils.js';
 
 const createSubject = document.getElementById('add');
+const createBody = document.getElementById('createBody');
 const createName = document.getElementById('createsubjectName');
 const createNameLabel = document.querySelector('label[for="createsubjectName"]');
 const createSubmit = document.getElementById('createSubmit');
+const createDay = document.getElementById('createDay');
+const createStart = document.getElementById('createStart');
+const createEnd = document.getElementById('createEnd');
+
+const sectionid = createSubject.dataset.sectionid;
+
+const error = document.createElement('div');
 
 // ============================================================================
 // 							FETCH API FOR BACKEND QUERY
 // ============================================================================
-//TODO: FIX THIS FOR TOMORROW.
-let result;
-const debounce_subjectName = debounce(async (id, subject_input) => {
+
+let avail;
+
+const debounce_subjectName = debounce(async (id, subject_input, day, start, end) => {
 	try {
-		const response = await axios.get(`/api/subject/${id}/${subject_input}`);
-		result = response.data.avail;
+		const response = await axios.get(`/api/subject/${id}`, {
+			params: { subject: subject_input, day: day, start: start, end: end },
+		});
+
+		avail = response.data.avail;
 	} catch (e) {
 		console.log(e);
 	}
 }, 350);
 
-createName.addEventListener('input', (e) => {
-	const val = createName.value;
-	if (val.trim().length > 0) {
-		createName.classList.remove('is-invalid');
-		createNameLabel.style.color = 'black';
-		createNameLabel.textContent = 'Subject Name';
-		debounce_subjectName(createSubject.dataset.sectionid, createName.value);
-	} else {
-		createName.classList.add('is-invalid');
-		createNameLabel.style.color = 'red';
-		createNameLabel.textContent = 'Invalid input';
-	}
+createEnd.addEventListener('input', (e) => {
+	const name = createName.value;
+	const day = createDay.value;
+	const start = createStart.value;
+	const end = createEnd.value;
+	debounce_subjectName(sectionid, name, day, start, end);
 });
-
+// FIXME: TO FIX
 createSubmit.addEventListener('click', (e) => {
-	e.preventDefault();
-	console.log(result);
+	const val = createName.value;
+
+	console.log(val);
 });
