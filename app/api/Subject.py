3624f8e1315b72e44c713f869b5e8a6fbf4cb543
Ranel John Tormis <62842207+R-esentful@users.noreply.github.com>
2023-02-03 17:22:33 +0800

    Updated Subject API

--- app/api/Subject.py
+++ app/api/Subject.py
@@ -7,15 +7,47 @@ from app.models.models import Subject,Section
 
 subject_api = Api(app)
 
-class SubjectByName():
+"""
+TO FIX!
+"""
+
+class SubjectAvailability(Resource):
     """
     API Resource for checking Subject per Section
     https://flask-restful.readthedocs.io/en/latest/index.html
     """
-    def get(self,name):
-        pass
+    subject_field = {
+        'Available':fields.String,
+    }
+    @marshal_with(subject_field)
+    def get(self,section_id,subject_name):
+        day = request.args.get('day')
+        start = request.args.get('start')
+        end = request.args.get('end')
 
+        if start != '' and end != '':
+            subject = Subject.query.filter_by(section_id = section_id,subject_name = subject_name).first();
+            queryStart = Subject.changeTime(Subject,start)
+            queryEnd = Subject.changeTime(Subject,end)
 
-subject_api.add_resource(SubjectByName,'/api/subjet/<string:section_name>/<string:subject_name>')
+            subject_start = Subject.query.filter_by(section_id = section_id , subject_day = day,subject_start = queryStart).first()
+            subject_end = Subject.query.filter_by(section_id = section_id , subject_day = day,subject_end = queryEnd ).first()
+
+            print(subject)
+            print(subject_start)
+            print(subject_end)
+            if all(avail is None for avail in (subject,subject_start,subject_end)):
+                result = {
+                    'Available':True
+                }
+                return result
+            else:
+                result = {
+                    
+                    'Available': False
+                }
+                return result
+
+subject_api.add_resource(SubjectAvailability,'/api/subject/<int:section_id>/<string:subject_name>')
 
 
