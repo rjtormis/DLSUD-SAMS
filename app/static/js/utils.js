54cd8c6890fb2590492c49a12032283c0c4d8bc7
Ranel John Tormis <62842207+R-esentful@users.noreply.github.com>
2023-02-04 14:58:02 +0800

    Updated Utils

--- app/static/js/utils.js
+++ app/static/js/utils.js
@@ -1,3 +1,10 @@
+export const emailRegex = (email) => {
+	const pattern = /@dlsud.edu.ph$/;
+	if (pattern.test(email)) {
+		return true;
+	}
+};
+
 // DEBOUNCING
 export function debounce(cb, delay = 1000) {
 	let timeout;
